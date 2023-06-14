2e5e28fa0f218fa2a2c55f4acb2832850385cbdc
enyaelvis <enyaelvis@gmail.com>
2023-06-14 11:40:28 +0100

    Js file added and scripts

---
+++ js/main.js
@@ -0,0 +1,38 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Code to handle form submission
+    alert('Form submitted successfully!');
+  });
+
+  document.querySelectorAll('a.nav-link').forEach(function(link) {
+    link.addEventListener('click', function(event) {
+      event.preventDefault();
+      const target = document.querySelector(link.getAttribute('href'));
+      target.scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+
+
+  document.querySelector('.navbar-toggler').addEventListener('click', function() {
+    const navbarCollapse = document.querySelector('.navbar-collapse');
+    navbarCollapse.classList.toggle('show');
+  });
+
+  const carousel = document.querySelector('#project-carousel');
+  const carouselInterval = setInterval(function() {
+    carousel.carousel('next');
+  }, 5000);
+
+  document.querySelectorAll('.social-icon').forEach(function(icon) {
+    icon.addEventListener('mouseenter', function() {
+      this.style.backgroundColor = '#000000';
+      this.style.color = '#ffffff';
+    });
+
+    icon.addEventListener('mouseleave', function() {
+      this.style.backgroundColor = '#ffffff';
+      this.style.color = '#000000';
+    });
+  });
